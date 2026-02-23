59f9e0d91eb45d8e2070404ba2f3279dd5e230ed
Samarth Agarwal <samarth@Samarths-MacBook-Air.local>
2026-02-23 17:34:04 +0800

    Redirect home page to /auction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
-import Navbar from "@/components/navbar";
-import Hero from "@/components/sections/hero";
-import Problem from "@/components/sections/problem";
-import Pillars from "@/components/sections/pillars";
-import Flywheel from "@/components/sections/flywheel";
-import Moats from "@/components/sections/moats";
-import CTA from "@/components/sections/cta";
-import Footer from "@/components/footer";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <Problem />
-        <Pillars />
-        <Flywheel />
-        <Moats />
-        <CTA />
-      </main>
-      <Footer />
-    </>
-  );
+  redirect('/auction');
 }
